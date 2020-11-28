ce3e649bd8f575a587f955480abcf3ab8db6c975
Tobias Bergmann <iibot@shell.bawue.net>
2020-11-28 21:38:49 +0100

    new croncopy

--- Adventskalender2020/nachher/croncopy.sh
+++ Adventskalender2020/nachher/croncopy.sh
@@ -3,6 +3,10 @@ cp ../vorher/??.html ..
 
 days=$(( (`date +%s` + 86400 - $( date -d 20201201 +%s )) / 86400 ))
 if (($days >24)); then days=24; fi
-echo `date`
-for i in `seq $days`; do echo $i; cp `printf %02d $i`.html ..; done
+echo `date` >>cron.log
+for i in `seq $days`; do 
+  echo $i >>cron.log;
+  cp `printf %02d $i`.html ..;
+done
 
+cat cron.log
